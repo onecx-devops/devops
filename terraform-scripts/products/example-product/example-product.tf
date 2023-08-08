40aa6c723960af9265fe4a567d29724846e041ea
pmcpg <101276471+pmcpg@users.noreply.github.com>
2023-08-08 13:13:00 +0200

    Update example-product.tf

--- terraform-scripts/products/example-product/example-product.tf
+++ terraform-scripts/products/example-product/example-product.tf
@@ -5,7 +5,7 @@ module "global" {
 module "example-repository" {
   source = "../../modules/repository"
 
-  repository_name = "example-repository"
+  repository_name = "example-repository-test"
   team_permission = "maintain"
   team_id         = module.example-team.team_id
   application_ids = module.global.applications_sonarcloud_id
